5da8a919c02cf8f8814bf1c95d67c8e54795a812
taichees <chiaki.621@gmail.com>
2026-06-18 15:45:45 +0900

    Initial Commit

---
+++ BulkReadGmailForiOS/BulkReadGmailForiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulkReadGmailForiOSApp.swift
+//  BulkReadGmailForiOS
+//
+//  Created by 飯田泰智 on 2026/06/18.
+//
+
+import SwiftUI
+
+@main
+struct BulkReadGmailForiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BulkReadGmailForiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BulkReadGmailForiOS
+//
+//  Created by 飯田泰智 on 2026/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
